8bbd4b47a41d4041194832b254a0b498be25f51d
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-07-18 13:46:19 +0530

    Toposort Corrected

--- Algorithms on Graphs/week2_decomposition2/2_toposort/toposort.cpp
+++ Algorithms on Graphs/week2_decomposition2/2_toposort/toposort.cpp
@@ -7,26 +7,11 @@ using namespace std;
 using std::pair;
 
 void dfs(vector<vector<int> > &adj, vector<int> &visited, vector<int> &order, int x) {
-  vector<int> stack;
-  stack.push_back(x);
-  vector<int> s;
-  while(!stack.empty()) {
-    int b = stack.back();
-    stack.pop_back();
-    for (int i =0; i < adj[b].size();i++) {
-      if (visited[adj[b][i]] == 0) stack.push_back(adj[b][i]); 
-    }
-    if (visited[b] == 0) {
-      s.push_back(b);
-      }
-    visited[b] = 1;
-    
+  visited[x] = 1;
+  for (int i = 0 ;i<adj[x].size();i++) {
+    if (!visited[adj[x][i]]) dfs(adj, visited, order, adj[x][i]);
   }
-  reverse(s.begin(),s.end());
-  for (int i =0; i< s.size();i++) {
-    order.push_back(s[i]);
-    }
-  //write your code here
+  order.push_back(x);
 }     
 
 vector<int> toposort(vector<vector<int> > adj) {
@@ -34,15 +19,11 @@ vector<int> toposort(vector<vector<int> > adj) {
   vector<int> order;
   
   for (int i = 0; i< adj.size(); i++) {
-    sort(adj[i].rbegin(),adj[i].rend());
-  }
-
-  for (int i = 0; i< adj.size(); i++) {
     if (visited[i] == 0) {
       dfs(adj,visited,order,i);
     }
   }
-  reverse(order.begin(), order.end());
+  reverse(order.begin(),order.end());
   return order;
 }
 
