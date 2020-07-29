4b64541a8fb27c7a59e3d90117e22088ff47ed7b
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-07-29 23:53:41 +0530

    Add readme

---
+++ Algorithmic Toolbox/README.md
@@ -0,0 +1,10 @@
+# Algorithmic Toolbox
+
+## Overview
+The course covers basic algorithmic techniques and ideas for computational problems arising frequently in practical applications: 
+1. sorting and searching, 
+2. divide and conquer, 
+3. greedy algorithms and, 
+4. dynamic programming.
+
+Problem set to each week's assignment can be found in the relevant week folder.
--- Algorithmic Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
+++ Algorithmic Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
 #include <cassert>
 
-// The following code calls a naive algorithm for computing a Fibonacci number.
-//
-// What to do:
-// 1. Compile the following code and run it on an input "40" to check that it is slow.
-//    You may also want to submit it to the grader to ensure that it gets the "time limit exceeded" message.
-// 2. Implement the fibonacci_fast procedure.
-// 3. Remove the line that prints the result of the naive algorithm, comment the lines reading the input,
-//    uncomment the line with a call to test_solution, compile the program, and run it.
-//    This will ensure that your efficient algorithm returns the same as the naive one for small values of n.
-// 4. If test_solution() reveals a bug in your implementation, debug it, fix it, and repeat step 3.
-// 5. Remove the call to test_solution, uncomment the line with a call to fibonacci_fast (and the lines reading the input),
-//    and submit it to the grader.
-
 int fibonacci_naive(int n) {
     if (n <= 1)
         return n;
@@ -40,8 +27,6 @@ void test_solution() {
 int main() {
     int n = 0;
     std::cin >> n;
-
-
     //std::cout << fibonacci_naive(n) << '\n';
     //test_solution();
     std::cout << fibonacci_fast(n) << '\n';
